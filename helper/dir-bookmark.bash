1534cc760c103120818251153aceb38f4d021d12
Mitmai <mitmai@gmail.com>
2013-08-23 13:33:50 +0800

    make abbr for bookmark command

--- helper/dir-bookmark.bash
+++ helper/dir-bookmark.bash
@@ -3,25 +3,25 @@ export BOOKMARK_PATH=$HOME/.dirbookmarks
 if [ ! -d $BOOKMARK_PATH ]; then
   mkdir $BOOKMARK_PATH
 fi
-function jump {
+function bj {
   cd -P "$BOOKMARK_PATH/$1" 2>/dev/null || echo "No such mark: $1"
 }
-function mark {
+function bm {
   mkdir -p "$BOOKMARK_PATH"; ln -s "$(pwd)" "$BOOKMARK_PATH/$1"
 }
-function unmark {
+function ubm {
   rm -i "$BOOKMARK_PATH/$1"
 }
-function marks {
+function bl {
 ls -l "$BOOKMARK_PATH" | sed 's/  / /g' | cut -d' ' -f9- | sed 's/ -/\t-/g' && echo
 }
 
-#_completemarks() {
-#  local curw=${COMP_WORDS[COMP_CWORD]}
-#  local wordlist=$(find $BOOKMARK_PATH -type l -printf "%f\n")
-#  COMPREPLY=($(compgen -W '${wordlist[@]}' -- "$curw"))
-#  return 0
-#}
-#
-#complete -F _completemarks jump unmark
+_completemarks() {
+  local curw=${COMP_WORDS[COMP_CWORD]}
+  local wordlist=$(find $BOOKMARK_PATH -type l -exec basename {} \; )
+  COMPREPLY=($(compgen -W '${wordlist[@]}' -- "$curw"))
+  return 0
+}
+
+complete -F _completemarks bj ubm
 
